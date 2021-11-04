33a706040de3364ba3dd546b6b6b0c2e99948eab
Rohan Bavishi <rbavishi@berkeley.edu>
2021-11-04 11:36:36 -0700

    Update zoom level to 5 in JS

--- jupyter-widget/viz_synthesis_widget/widget.py
+++ jupyter-widget/viz_synthesis_widget/widget.py
@@ -25,7 +25,7 @@ class VizSynthesisWidget(widgets.DOMWidget):
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
     data = List(default_value=[]).tag(sync=True)
-    num_cols = Int(3).tag(sync=True)
+    num_cols = Int(5).tag(sync=True)
     selection = Int(-1).tag(sync=True)
 
     def clear(self):
